c7e3561b46eac6ac637db958f34d3d30cf52bf97
Shawna <shawnalehman@mail.com>
2017-05-11 16:25:50 -0400

    login feature complete

--- week2/w2d2/tiny-app/express-server.js
+++ week2/w2d2/tiny-app/express-server.js
@@ -1,29 +1,34 @@
 const express = require("express");
+
 const app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+
+var PORT = process.env.PORT || 8080;
+
 const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded({extended: true}));
+
+const cookieParser = require('cookie-parser');
+
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.set("view engine", "ejs");
-
-// var urlDatabase = {
-// "b2xVn2":{ shortenedURL: "b2xVn2", longURL: "http://www.lighthouselabs.ca"},
-// "9sm5xK":{ shortenedURL: "9sm5xK", longURL: "http://www.google.com"}
-// };
 
 var urlDatabase = {
 "b2xVn2": "http://www.lighthouselabs.ca",
 "9sm5xK" : "http://www.google.com"
 };
 
+var userDatabase = {};
+
+
+
+
 function generateRandomString(length, chars) {
     var result = '';
     for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
     return result;
 }
-// var req.params = {
-//   "id" : "b2xVn2"
-// }
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -35,32 +40,24 @@ app.listen(PORT, () => {
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  res.render("urls_new", {username: req.cookies["username"]});
 });
-
 
 //new url and assign random string
 app.post("/urls", (req, res) => {
   let newShortURL = generateRandomString(6, '0123456789abcdefghijklmnOPQRSTUVWXYZ')
-  // console.log(req.body);  // debug statement to see POST parameters
-  urlDatabase[newShortURL] = req.body.longURL;  // Respond with 'Ok' (we will replace this)
+  urlDatabase[newShortURL] = req.body.longURL;
   res.redirect("/urls/" + newShortURL);
 });
 
-//get all urls
+//get all urls and stores current user name to urls/index /header
 app.get("/urls", (req, res) => {
-  // let templateVars = ;
-  //console.log(req)
-  //console.log(urlDatabase);
-  res.render("urls_index", { urlDatabase: urlDatabase });
+  res.render("urls_index", { urlDatabase: urlDatabase, username: req.cookies["username"]});
 });
 
 //getting one url
 app.get("/urls/:id", (req, res) => {
-  // let templateVars = ;
-   //console.log(urlDatabase);
-  // res.render("urls_show", {})
-  res.render("urls_show", { newShortURL: req.params.id, longURL: urlDatabase[req.params.id] });
+  res.render("urls_show", { newShortURL: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"]});
 });
 
 //redirecting short link to long link page
@@ -69,16 +66,13 @@ app.get("/u/:shortURL", (req, res) => {
   res.redirect(longURL);
 });
 
-
 // deletes a link <%=`/urls/${url}/delete`% matches the form input >
 app.post('/urls/:url/delete', (req, res) =>{
   delete urlDatabase[req.params.url];
   res.redirect('/urls');
-
 });
 
 //app.post('urls/:url/update', (req, res))
-
 app.post('/urls/:url/updates', (req, res) => {
   let updatedURL = req.body.longURL;
   urlDatabase[req.params.url] = updatedURL;
@@ -86,11 +80,19 @@ app.post('/urls/:url/updates', (req, res) => {
   res.redirect('/urls');
  });
 
-// <form mathod="POST" action= "/urls"
-//       <label for="longURL">Enter a URL:</label>
-//       <input id="longURL" type="text" name="longURL" placeholder="http://" style="width: 300px">
-//       <input type="submit" value="UPDATE">
-//     </form>
+//svaes input username to an object and sets a cookie
+app.post('/login', (req, res) => {
+  let username = req.body.username;
+  res.cookie("username", req.body.username);
+  res.redirect('/urls')
+});
+
+
+app.post('/logout', (req, res) => {
+  res.clearCookie("username")
+  res.redirect('/urls')
+});
+
 
 
 
