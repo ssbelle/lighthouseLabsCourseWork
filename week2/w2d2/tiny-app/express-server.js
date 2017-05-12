cfbea76eaab4b4a0acaebd7f69df989ef984628e
Shawna <shawnalehman@gmail.com>
2017-05-12 18:56:17 +0000

    combined register and login features with cookie

--- week2/w2d2/tiny-app/express-server.js
+++ week2/w2d2/tiny-app/express-server.js
@@ -21,7 +21,20 @@ var urlDatabase = {
 
 var userDatabase = {};
 
-
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    name: "Rosy",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID",
+    name: "Bob",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
 
 
 function generateRandomString(length, chars) {
@@ -30,9 +43,37 @@ function generateRandomString(length, chars) {
     return result;
 }
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/register", (req, res) => {
+  res.render("register");
 });
+
+app.post("/register", (req, res) =>{
+  let checkEmails = Object.keys(users).filter(key => users[key].email == req.body.email)
+  if(checkEmails.length > 0){
+    res.status(400).send('This email is already registered.')
+  } else {
+    let user_id = generateRandomString(3, '0123456789abcdefghijklmnOPQRSTUVWXYZ')
+    users[user_id] = {
+      id: user_id,
+      name: req.body.email,
+      email: req.body.email,
+      password: req.body.password
+    }
+    if(users[user_id].email.length === 0){
+      res.status(400).send('Please enter a valid email!');
+    }
+    if(users[user_id].password.length < 6){
+      res.status(400).send('Please enter a password at least 6 characters in length!')
+    }
+    res.cookie("user_id", user_id)
+    //console.log(req.cookies["user_id"], users, users[req.cookies["user_id"]])
+    res.redirect("/urls");
+  }
+});
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -40,7 +81,7 @@ app.listen(PORT, () => {
 
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new", {username: req.cookies["username"]});
+  res.render("urls_new", {user: users[req.cookies["user_id"]]});
 });
 
 //new url and assign random string
@@ -52,12 +93,12 @@ app.post("/urls", (req, res) => {
 
 //get all urls and stores current user name to urls/index /header
 app.get("/urls", (req, res) => {
-  res.render("urls_index", { urlDatabase: urlDatabase, username: req.cookies["username"]});
+  res.render("urls_index", { urlDatabase: urlDatabase, user: req.cookies["user"]});
 });
 
-//getting one url
+//getting one url reading the signed in user from the cookie
 app.get("/urls/:id", (req, res) => {
-  res.render("urls_show", { newShortURL: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"]});
+  res.render("urls_show", { newShortURL: req.params.id, longURL: urlDatabase[req.params.id], user: req.cookies["user_id"]});
 });
 
 //redirecting short link to long link page
@@ -76,20 +117,30 @@ app.post('/urls/:url/delete', (req, res) =>{
 app.post('/urls/:url/updates', (req, res) => {
   let updatedURL = req.body.longURL;
   urlDatabase[req.params.url] = updatedURL;
-  console.log(updatedURL)
   res.redirect('/urls');
  });
 
-//svaes input username to an object and sets a cookie
+app.get('/login', (req, res) => {
+  res.render('login')
+
+})
+
+//saves input username to an object and sets a cookie function  that compares let email = req.body.email; with user: req.cookies["user_id"]
 app.post('/login', (req, res) => {
-  let username = req.body.username;
-  res.cookie("username", req.body.username);
+  for (var user in users){
+    if(users[user].email === req.body.email && users[user].password === req.body.password){
+     res.cookie("user", users[user]);
+    }
+    else {
+      res.status(403).send('Your user name or password do not match.<br> Please try again or <a href="/register">Register Here!</a>')
+  }
   res.redirect('/urls')
+}
 });
 
 
 app.post('/logout', (req, res) => {
-  res.clearCookie("username")
+  res.clearCookie("user")
   res.redirect('/urls')
 });
 
